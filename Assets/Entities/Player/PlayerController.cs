e90d4077313c6eae690bae67d0046668f7f95608
Mubashwer <mubashwersk@gmail.com>
2015-08-18 22:55:30 +1000

    Added player shooting and bullet shredding

--- Assets/Entities/Player/PlayerController.cs
+++ Assets/Entities/Player/PlayerController.cs
@@ -6,6 +6,10 @@ public class PlayerController : MonoBehaviour {
 	//  Variables for restricting movement
 	private float xMin, xMax, yMin, yMax, padding = 0.5f;
 	
+	public GameObject shortBullet;
+	public float shortBulletSpeed = 10;
+	public float shortBulletShootRate = 0.15f;
+	
 	// Use this for initialization
 	void Start () {
 		
@@ -20,18 +24,40 @@ public class PlayerController : MonoBehaviour {
 	// Update is called once per frame
 	void Update() {
 		
+		// Fire bullet at a fixed rate if screen is touched or space is pressed
+		if((Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Began) ||
+			Input.GetKeyDown(KeyCode.Space)) {
+			InvokeRepeating("ShootShortBullet", 0.00001f, shortBulletShootRate);
+		}
+		if(((Input.touchCount > 0 && (Input.touches[0].phase == TouchPhase.Ended ||
+			Input.touches[0].phase == TouchPhase.Canceled) )) || 
+			Input.GetKeyUp(KeyCode.Space)) {
+			CancelInvoke("ShootShortBullet");
+		}
+		// Follow touch swipe or mouse left-click
 		FollowSwipe ();
 	}
 	
 	
+	
+	// Just shoot a bullet
+	void ShootShortBullet() {
+		Vector3 bulletPos = transform.position;
+		bulletPos.y += 0.5f;
+		GameObject laserBeam = Instantiate(shortBullet, bulletPos, Quaternion.identity) as GameObject;
+		laserBeam.GetComponent<Rigidbody2D>().velocity = Vector3.up * shortBulletSpeed;		
+	}
+	
+	// Move with same velocity as touch swipe
 	private void FollowSwipe() {
-		
-		// Move with same velocity as touch swipe
-		if(Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Moved) { // Check for swipe
-			Vector2 delta = Input.touches[0].deltaPosition; // touch displacement
-			Vector3 delta3 = new Vector3(delta.x, delta.y, transform.position.z);
-			// move in the direction and distance of touch displacement in world space
-			transform.Translate(delta3 * Time.deltaTime, Space.World);
+		if(Input.touchCount > 0) { 
+			// Check for any touch swipe
+			for(int i = 0; i < Input.touchCount && Input.touches[i].phase == TouchPhase.Moved; i++) {
+				Vector2 delta = Input.touches[i].deltaPosition; // touch displacement
+				Vector3 delta3 = new Vector3(delta.x, delta.y, transform.position.z);
+				// move in the direction and distance of touch displacement in world space
+				transform.Translate(delta3 * Time.deltaTime, Space.World);
+			}
 		} 
 		// otherwise move to left-click position of mouse
 		else if (Input.touchCount <= 0 && Input.GetMouseButton(0)){
@@ -43,6 +69,7 @@ public class PlayerController : MonoBehaviour {
 		float newX = Mathf.Clamp (transform.position.x, xMin, xMax);
 		float newY = Mathf.Clamp (transform.position.y, yMin, yMax);
 		transform.position = new Vector3(newX, newY, transform.position.z);
+
 	}
 
 }
---
+++ Assets/Entities/Projectiles/Shredder.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shredder : MonoBehaviour {
+
+	// Destroy objects e.g bullets when the enter shredder
+	// outside screen
+	void OnTriggerEnter2D(Collider2D collider) {
+		Destroy (collider.gameObject);
+	}
+}
