5ca76457bf269caa2514a44e90ff7ed7b8dac466
crozone <crozone_xz@hotmail.com>
2015-10-07 14:28:20 +1100

    Added updates

--- Assets/Scripts/PopulateLeaderboard.cs
+++ Assets/Scripts/PopulateLeaderboard.cs
@@ -12,8 +12,7 @@ public class PopulateLeaderboard : MonoBehaviour {
         WWW www = new WWW("http://localhost:8080/Api/GetScores");
         
         string rawText = www.text
-            ?? "[{\"Id\":1,\"UserUniqueKey\":\"123\",\"UserName\":\"TESTNAME\","
-            + "\"Score\":12345,\"Time\":\"2015 - 10 - 07T09: 56:45.4148302\"}]";
+            ?? "[]";
 
         Debug.Log("Parsing high scores JSON data");
         JSONNode node = JSON.Parse(rawText);
@@ -24,7 +23,7 @@ public class PopulateLeaderboard : MonoBehaviour {
             int score = scoreRecord["Score"].AsInt;
             string TimeString = scoreRecord["Time"].Value;
 
-
+            Instantiate(new 
 
             // TODO: Populate the UI with the downloaded leaderboard information
 
