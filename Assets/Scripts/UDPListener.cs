f2862e0335cc8719de981f969f5a47bd4433caf4
crozone <crozone_xz@hotmail.com>
2015-10-20 14:21:35 +1100

    Updated read code

--- Assets/Scripts/UDPListener.cs
+++ Assets/Scripts/UDPListener.cs
@@ -8,29 +8,26 @@ using System;
 using UnityEngine.UI;
 
 public class UDPListener : MonoBehaviour {
-
-    Thread receiveThread;
-
     private readonly object infoLock = new object();
     private string lastData = "";
 
+    UdpClient client;
+
     // start from unity3d
     public void Start() {
-        receiveThread = new Thread(new ThreadStart(ReceiveData));
-        receiveThread.IsBackground = true;
-        receiveThread.Start();
+        client = new UdpClient(7778);
+        client.Client.ReceiveTimeout = 100;
+        StartCoroutine(ReceiveDataCoRoutine());
     }
 
     public void Update() {
         var text = GameObject.Find("UDPTestText").GetComponent<Text>();
-        lock(infoLock) {
+        lock (infoLock) {
             text.text = lastData;
         }
     }
 
-    public void ReceiveData() {
-        UdpClient client = new UdpClient(7778);
-
+    IEnumerator ReceiveDataCoRoutine() {
         while (true) {
             try {
                 IPEndPoint anyIP = new IPEndPoint(IPAddress.Any, 0);
@@ -43,10 +40,12 @@ public class UDPListener : MonoBehaviour {
                     lastData = text;
                 }
             }
+            catch (SocketException) {
+            }
             catch (Exception e) {
                 print(e.ToString());
             }
+            yield return new WaitForSeconds(1);
         }
-
     }
 }
