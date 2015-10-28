7ee793fe0611bccdec2d9566da978d24c831b690
Mubashwer <mubashwersk@gmail.com>
2015-10-28 22:51:13 +1100

    fix options

---
+++ Assets/Scripts/Options.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Options : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        GetComponentInChildren<Slider>().value = (int)GameManager.instance.CurrentPlayerDifficulty;
+        GetComponentInChildren<Toggle>().isOn = !LevelManager.instance.GetComponent<AudioSource>().mute;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
